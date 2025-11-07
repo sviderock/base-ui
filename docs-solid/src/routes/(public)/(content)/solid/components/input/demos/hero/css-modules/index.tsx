395eb5368b1cc4ce0d5f03635d935e074a7cdc31
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-11-07 15:43:36 +0200

    [docs-solid]: Add input component

---
+++ docs-solid/src/routes/(public)/(content)/solid/components/input/demos/hero/css-modules/index.tsx
@@ -0,0 +1,6 @@
+import { Input } from '@base-ui-components/solid/input';
+import styles from './index.module.css';
+
+export default function ExampleInput() {
+  return <Input placeholder="Name" class={styles.Input} />;
+}
