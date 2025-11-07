11cbbe10b68e662331716fa98aa89b66bd4c85db
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-11-07 16:04:10 +0200

    [docs-solid]: Add separator component

---
+++ docs-solid/src/routes/(public)/(content)/solid/components/separator/demos/hero/css-modules/index.tsx
@@ -0,0 +1,30 @@
+import { Separator } from '@base-ui-components/solid/separator';
+import styles from './index.module.css';
+
+export default function ExampleSeparator() {
+  return (
+    <div class={styles.Container}>
+      <a href="#" class={styles.Link}>
+        Home
+      </a>
+      <a href="#" class={styles.Link}>
+        Pricing
+      </a>
+      <a href="#" class={styles.Link}>
+        Blog
+      </a>
+      <a href="#" class={styles.Link}>
+        Support
+      </a>
+
+      <Separator orientation="vertical" class={styles.Separator} />
+
+      <a href="#" class={styles.Link}>
+        Log in
+      </a>
+      <a href="#" class={styles.Link}>
+        Sign up
+      </a>
+    </div>
+  );
+}
---
+++ docs-solid/src/routes/(public)/(content)/solid/components/separator/demos/hero/tailwind/index.tsx
@@ -0,0 +1,47 @@
+import { Separator } from '@base-ui-components/solid/separator';
+
+export default function ExampleSeparator() {
+  return (
+    <div class="flex gap-4 text-nowrap">
+      <a
+        href="#"
+        class="text-sm text-gray-900 decoration-gray-400 decoration-1 underline-offset-2 outline-none hover:underline focus-visible:rounded-sm focus-visible:no-underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800"
+      >
+        Home
+      </a>
+      <a
+        href="#"
+        class="text-sm text-gray-900 decoration-gray-400 decoration-1 underline-offset-2 outline-none hover:underline focus-visible:rounded-sm focus-visible:no-underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800"
+      >
+        Pricing
+      </a>
+      <a
+        href="#"
+        class="text-sm text-gray-900 decoration-gray-400 decoration-1 underline-offset-2 outline-none hover:underline focus-visible:rounded-sm focus-visible:no-underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800"
+      >
+        Blog
+      </a>
+      <a
+        href="#"
+        class="text-sm text-gray-900 decoration-gray-400 decoration-1 underline-offset-2 outline-none hover:underline focus-visible:rounded-sm focus-visible:no-underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800"
+      >
+        Support
+      </a>
+
+      <Separator orientation="vertical" class="w-px bg-gray-300" />
+
+      <a
+        href="#"
+        class="text-sm text-gray-900 decoration-gray-400 decoration-1 underline-offset-2 outline-none hover:underline focus-visible:rounded-sm focus-visible:no-underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800"
+      >
+        Log in
+      </a>
+      <a
+        href="#"
+        class="text-sm text-gray-900 decoration-gray-400 decoration-1 underline-offset-2 outline-none hover:underline focus-visible:rounded-sm focus-visible:no-underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800"
+      >
+        Sign up
+      </a>
+    </div>
+  );
+}
