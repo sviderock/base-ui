9063d8038bc887c39fe6a61692b3bdf613229621
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2025-12-31 18:20:18 +0200

    another try

--- docs-solid/src/components/Demo/DemoLoader.tsx
+++ docs-solid/src/components/Demo/DemoLoader.tsx
@@ -1,7 +1,18 @@
-import { createAsync } from '@solidjs/router';
-import { Show, splitProps, Suspense, type ComponentProps } from 'solid-js';
+import { createAsync, query } from '@solidjs/router';
+import { createMemo, Show, splitProps, Suspense, type ComponentProps } from 'solid-js';
 import { Demo } from './Demo';
 import { loadDemo } from './loadDemo';
+
+const getVariants = query(async (path: string) => {
+  'use server';
+  const variants = await loadDemo(path);
+
+  if (!variants.length) {
+    throw new Error(`\nCould not load demo: no demos found in "${path}".`);
+  }
+
+  return variants;
+}, 'components-metadata');
 
 export interface DemoLoaderProps extends Omit<ComponentProps<typeof Demo>, 'variants'> {
   /** Absolute path to a folder with demos or to a .tsx file with the main demo */
@@ -12,27 +23,21 @@ export interface DemoLoaderProps extends Omit<ComponentProps<typeof Demo>, 'vari
 
 export function DemoLoader(componentProps: DemoLoaderProps) {
   const [local, props] = splitProps(componentProps, ['path', 'scope']);
-  const variants = createAsync(async () => {
-    console.log({ url: import.meta.url });
-    console.log('loadDemo', local.path);
-    const variants = await loadDemo(local.path);
-    console.log('variants', variants);
-    for (const variant of variants) {
-      variant.component = local.scope[variant.component];
-    }
-    console.log('variants', variants);
+  const variants = createAsync(() => getVariants(local.path));
 
-    if (!variants.length) {
-      throw new Error(`\nCould not load demo: no demos found in "${local.path}".`);
-    }
-
-    return variants;
+  const variantsWithComponents = createMemo(() => {
+    return variants()?.map((variant) => {
+      return {
+        ...variant,
+        component: local.scope[variant.component],
+      };
+    });
   });
 
   return (
     <Suspense>
-      <Show when={variants()?.length}>
-        <Demo variants={variants()!} {...props} />
+      <Show when={variantsWithComponents()?.length}>
+        <Demo variants={variantsWithComponents()!} {...props} />
       </Show>
     </Suspense>
   );
